b9a8bf7844740f38eca2e7b91d9cef0b6ab0bade
Nicolas David Peñuela Caballero <nicolas.penuela@mininterior.local>
2019-07-29 11:17:38 -0500

    Updates

---
+++ precache-manifest.9afe02a64f09190f1b887e4a2dbe2881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660d87c3cede4da61bb54a3c7687d5f0",
+    "url": "/palette-colors-app/index.html"
+  },
+  {
+    "revision": "2909bde9457fd5d45ad7",
+    "url": "/palette-colors-app/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "313325582206be4fb435",
+    "url": "/palette-colors-app/static/css/main.037ebe99.chunk.css"
+  },
+  {
+    "revision": "2909bde9457fd5d45ad7",
+    "url": "/palette-colors-app/static/js/2.fe96a94d.chunk.js"
+  },
+  {
+    "revision": "313325582206be4fb435",
+    "url": "/palette-colors-app/static/js/main.7dc6fa8d.chunk.js"
+  },
+  {
+    "revision": "685b3917276cd80cfbf6",
+    "url": "/palette-colors-app/static/js/runtime~main.43599ef0.js"
+  }
+]);
